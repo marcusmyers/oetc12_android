31bafe5d1d441c88d7d53c0f5cc8409fbce1de8e
Mark Myers <marcusmyers@gmail.com>
2012-02-01 08:29:13 -0500

    New default layout

--- gen/org/oetc/oetc12_android/R.java
+++ gen/org/oetc/oetc12_android/R.java
@@ -11,7 +11,16 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int computer=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int rss=0x7f020002;
+        public static final int telephone=0x7f020003;
+    }
+    public static final class id {
+        public static final int buttonSpacer=0x7f050003;
+        public static final int tableLayout1=0x7f050000;
+        public static final int tableRow1=0x7f050001;
+        public static final int tableRow2=0x7f050002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
