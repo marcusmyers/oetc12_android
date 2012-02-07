1c21b87b9fbf116252edc40fc6a9894b185fb0e3
Mark Myers <marcusmyers@gmail.com>
2012-02-07 11:03:37 -0500

    Removed ListFont style

--- gen/org/oetc/oetc12_android/R.java
+++ gen/org/oetc/oetc12_android/R.java
@@ -18,22 +18,22 @@ public final class R {
         public static final int telephone=0x7f020004;
     }
     public static final class id {
-        public static final int back=0x7f06000f;
-        public static final int buttonContact=0x7f060004;
-        public static final int buttonRss=0x7f060005;
-        public static final int buttonSpacer=0x7f060008;
-        public static final int buttonWeb=0x7f060006;
-        public static final int details=0x7f06000d;
-        public static final int feedTitle=0x7f06000c;
-        public static final int linearLayout1=0x7f060000;
-        public static final int listContact=0x7f060001;
-        public static final int storybox=0x7f06000e;
-        public static final int tableLayout1=0x7f060002;
-        public static final int tableRow1=0x7f060003;
-        public static final int tableRow2=0x7f060007;
-        public static final int telContact=0x7f060009;
-        public static final int textContactName=0x7f06000a;
-        public static final int textNumber=0x7f06000b;
+        public static final int back=0x7f05000f;
+        public static final int buttonContact=0x7f050004;
+        public static final int buttonRss=0x7f050005;
+        public static final int buttonSpacer=0x7f050008;
+        public static final int buttonWeb=0x7f050006;
+        public static final int details=0x7f05000d;
+        public static final int feedTitle=0x7f05000c;
+        public static final int linearLayout1=0x7f050000;
+        public static final int listContact=0x7f050001;
+        public static final int storybox=0x7f05000e;
+        public static final int tableLayout1=0x7f050002;
+        public static final int tableRow1=0x7f050003;
+        public static final int tableRow2=0x7f050007;
+        public static final int telContact=0x7f050009;
+        public static final int textContactName=0x7f05000a;
+        public static final int textNumber=0x7f05000b;
     }
     public static final class layout {
         public static final int contact=0x7f030000;
@@ -44,13 +44,10 @@ public final class R {
         public static final int showdescription=0x7f030005;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int details=0x7f050004;
-        public static final int feedTitle=0x7f050003;
-        public static final int titleContact=0x7f050001;
-        public static final int titleRss=0x7f050002;
-    }
-    public static final class style {
-        public static final int ListFont=0x7f040000;
+        public static final int app_name=0x7f040000;
+        public static final int details=0x7f040004;
+        public static final int feedTitle=0x7f040003;
+        public static final int titleContact=0x7f040001;
+        public static final int titleRss=0x7f040002;
     }
 }
